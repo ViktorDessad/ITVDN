3c81a9e904b7039dfff31e514b51b279ea1dbab0
Віктор Шереш <sheregi1000@gmail.com>
2019-09-13 16:55:34 +0300

    Methods

---
+++ 008_Methods/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static int Calculate(int a, int b, int c)
+        {
+            b = b + 1;
+            c = c + 1;
+            return a + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            int aa = Calculate(a, b, c);
+            Console.WriteLine("{0} {1} {2}", aa, b, c);
+        }
+    }
+}
