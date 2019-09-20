7b4299951c5d90092a3a117a3fb84912555f9cae
Віктор Шереш <sheregi1000@gmail.com>
2019-09-21 01:05:49 +0300

    Recursion

---
+++ 008_Methods/Tast_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tast_3
+{
+    class Program
+    {
+        static void Recursion(int counter)
+        {
+            counter--;
+            Console.WriteLine("First method part: {0}", counter);
+
+            if (counter !=0)
+            {
+                Recursion(counter);
+            }
+            Console.WriteLine("Second method part: {0}", counter);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Recursion(3);
+            Console.ReadKey();
+        }
+    }
+}
