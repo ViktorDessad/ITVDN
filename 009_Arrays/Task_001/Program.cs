efdb30b0a2bd3bea613d53011f8a6b9931227427
Віктор Шереш <sheregi1000@gmail.com>
2019-09-21 01:08:04 +0300

    Arrays elements sum

--- 009_Arrays/Task_001/Program.cs
+++ 009_Arrays/Task_001/Program.cs
@@ -8,32 +8,10 @@ namespace Task_001
 {
     class Program
     {
-        static int SumArguments(out int [] arr)
-        {
-            int argumentNum = Int32.Parse(Console.ReadLine());
-            int[] array = new int[argumentNum];
-            arr = new int [argumentNum];
-            int sum = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = i + 1;
-                sum += array[i];
-                arr [i] = array[i];
-            }
-            return sum;
-        }
+       
+        do
+	{
 
-        static void Main(string[] args)
-        {
-            Console.Write("Enter a number: ");
-            int [] arr;
-            int res = SumArguments(out arr);
-            Console.WriteLine("The result = {0} ", res);
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-            Console.WriteLine(arr[i]);
-            }
-        }
+	} while (false);
     }
 }
