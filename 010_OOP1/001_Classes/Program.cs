862182f4e7d6d259b63f6e6cc7f265baee35c049
Віктор Шереш <sheregi1000@gmail.com>
2019-09-28 19:01:14 +0300

    Methods and fields

---
+++ 010_OOP1/001_Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Classes
+{
+    class MyClass
+    {
+        public string field;
+
+        public void HelloWorld()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass helloWorld = new MyClass();
+
+            helloWorld.field = "Hello World";
+            helloWorld.HelloWorld();
+            Console.WriteLine(helloWorld.field);
+        }
+    }
+}
