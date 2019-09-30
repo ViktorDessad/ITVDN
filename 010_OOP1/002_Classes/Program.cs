619edb05e769b11e8a3e5b4d324d7bedb89b2acc
Віктор Шереш <sheregi1000@gmail.com>
2019-09-30 19:15:07 +0300

    Weak and strong reference

---
+++ 010_OOP1/002_Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Classes
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field); ;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance1 = new MyClass();
+            MyClass instance2 = new MyClass();
+
+            // Обращение к членам класса по сильной ссылке
+            instance1.field = 2;
+            instance2.field = 5;
+
+            instance1.Method();
+            instance2.Method();
+
+            // Обращение к членам класса по слабой ссылке
+            new MyClass().Method();
+            new MyClass().field = 2;
+        }
+    }
+}
