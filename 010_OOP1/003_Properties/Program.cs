fe82c857631d5b4c862689d61efc16094fb7fcb0
Віктор Шереш <sheregi1000@gmail.com>
2019-10-01 23:37:46 +0300

    SetField and GetField methods

---
+++ 010_OOP1/003_Properties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Properties
+{
+    class MyClass
+    {
+        private string field = "Hello Viktor";
+
+        public void SetField(string value)
+        {
+            field = value;
+        }
+        public string GetField()
+        {
+            return field;
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            instance.SetField("Hello World");
+
+            string @string = instance.GetField();
+
+            Console.WriteLine(@string);
+            Console.ReadKey();
+        }
+    }
+}
