b8f83102283394d45b01781dc2efb8e4dec93f90
Віктор Шереш <sheregi1000@gmail.com>
2019-10-01 23:38:32 +0300

    Properties (set, get)

---
+++ 010_OOP1/004_Properties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Properties
+{
+    class MyClass
+    {
+        private string field = null;
+        // Название поля field должно совпадать с именем свойства данного поля Field 
+        // и разница только в заглавной букве, она должна быть большой - Field
+        public string Field // Открытое строковое свойство Field
+        {
+            set
+            {
+                field = value; // Method-mutator
+            }
+            get
+            {
+                return field; // Method-accessor
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            // Обращение к свойству Field со следующим присвоением полю ему значение 
+            instance.Field = "Hello World"; // Method-mutator 
+
+            string @string = instance.Field; // Method-accessor
+            Console.WriteLine(@string);
+            Console.ReadKey();
+        }
+    }
+}
