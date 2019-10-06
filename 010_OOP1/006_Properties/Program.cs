eb6b5247a38d8c08d29c6c202ca84106c7a1da9d
Віктор Шереш <sheregi1000@gmail.com>
2019-10-06 23:39:39 +0300

    Getter and Setter

---
+++ 010_OOP1/006_Properties/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Properties
+{
+    class Str 
+    {
+        private string str = null;
+
+        public string Property
+        {
+            set
+            {               
+                    if (value == "fool")
+                    {
+                       Console.WriteLine("This is a bad world, try once again");
+                    }
+                    else
+                    {                   
+                        str = value;
+                    }    
+            }
+
+            get
+            {
+                if (str == "Hello")
+	            {
+                    return str;
+	            }
+                else if (str == " ")
+            	{
+                    return "Space";
+	            }
+                else if (str == "Hello Viktor")
+                {
+                    return str.ToUpper();
+                }
+                else
+                {
+                    return "null";
+                }
+
+            }
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         Str @string = new Str();
+            Console.Write("Enter a word: ");
+            @string.Property = Console.ReadLine();
+            Console.WriteLine(@string.Property);
+            Console.ReadKey();
+        }
+    }
+}
