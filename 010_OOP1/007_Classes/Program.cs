a4964a6cdb03bc004c5adefc480f0f4f21283b26
Віктор Шереш <sheregi1000@gmail.com>
2019-10-26 14:41:12 +0300

    Identify the word (test)

---
+++ 010_OOP1/007_Classes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Classes
+{
+    class WordIdentify
+    {
+        private string word = null;
+
+        public void SetWord(string value)
+        {
+            if (value == "fool")
+            {
+                word = value;
+                Console.WriteLine("This is a bad word {0}", word);
+            }
+            else
+            {
+                word = value;
+                Console.WriteLine("This is a good word {0}", word);
+            }
+        }
+
+        public string GetWord()
+        {
+            return word;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            WordIdentify word = new WordIdentify();
+            Console.Write("Enter a word: ");
+            word.SetWord(Console.ReadLine());
+            string str = word.GetWord();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
