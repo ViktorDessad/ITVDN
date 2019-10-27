855162bf4318442a29519ade6d1660d4e12b824b
Віктор Шереш <sheregi1000@gmail.com>
2019-10-27 22:56:11 +0200

    Properties

---
+++ 010_OOP1/010_Properties/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Properties
+{
+    class Constants
+    {
+        private double pi = 3.14D;
+        private double e = 2.71D;
+
+        public double PI
+        {
+            set
+            {
+                pi = value;
+            }
+        }
+
+        public double E
+        {
+            get
+            {
+                return e;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constants constants = new Constants();
+            //Console.WriteLine(constants.PI); // WhiteOnly
+            constants.PI = 213.231D;
+            //constants.E = 123.D; // ReadOnly
+            Console.WriteLine(constants.E);
+
+
+        }
+    }
+}
