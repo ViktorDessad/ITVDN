5ce6af6aee5c8e75cfdd4ec1a1d902c90a9de750
Віктор Шереш <sheregi1000@gmail.com>
2019-10-27 22:56:58 +0200

    Properties

---
+++ 008_Methods/Task_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static int Pow(int x, int y = 2)
+        {
+            int res = 1;
+            for (int i = 0; i < y; i++)
+            {
+                res *= x;
+            }
+
+            return res;
+        }
+
+        static double Method(int x, double y)
+        {
+            double res = x + y;
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            //double res = Method(10, 20.54);
+            //Console.WriteLine(res);
+            var res = Pow(10);
+            Console.WriteLine(res);
+
+        }
+    }
+}
