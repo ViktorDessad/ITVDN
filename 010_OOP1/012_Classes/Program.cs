afb46b9cce7338b73f0f6dd217fa00a7bf2c44f0
Віктор Шереш <sheregi1000@gmail.com>
2019-10-28 20:44:00 +0200

    Classes

---
+++ 010_OOP1/012_Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_Classes
+{
+    class Buildings
+    {
+        public int floors;
+        public int occupants;
+        public int area;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buildings buildings = new Buildings();  
+            Buildings buildings1 = buildings;       
+            buildings.area = 50;
+            buildings1.area = 999;
+            Console.WriteLine($"{buildings.area},{buildings1.area}");
+        }
+    }
+}
