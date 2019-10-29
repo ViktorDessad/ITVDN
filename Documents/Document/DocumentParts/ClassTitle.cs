6bf0e4a1429cc7e8533bff39bfd7b4a1914d7eaf
Віктор Шереш <sheregi1000@gmail.com>
2019-10-29 21:39:35 +0200

    Simple document creating

---
+++ Documents/Document/DocumentParts/ClassTitle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents
+{
+    class ClassTitle
+    {
+        private string titleName;
+
+        public string Title
+        {
+            get 
+            {
+                if (titleName !=null)
+                {
+                    return titleName;
+                }
+                else
+                {
+                    return "The title is empty";
+                }
+            }
+
+            set
+            {
+                titleName = value;
+            }
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Title);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
