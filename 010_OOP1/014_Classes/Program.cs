a1a8fc0c2aff7b5a921b2ff9987d81b2a271469e
Віктор Шереш <sheregi1000@gmail.com>
2019-10-29 15:17:11 +0200

    Class calling from another class

---
+++ 010_OOP1/014_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_Classes
+{
+    class MyClass
+    {
+        public void CallingMethod()
+        {
+            Console.WriteLine("This class calling from another class");
+        }
+    }
+
+    class MyClass2
+    {
+        public void Calling(MyClass my)
+        {
+            my.CallingMethod();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            MyClass2 my2 = new MyClass2();
+            my2.Calling(my);
+        }
+    }
+}
