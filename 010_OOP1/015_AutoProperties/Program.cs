5cf0e38b9479c6eb4ba6fa05d8124832e8bd5574
Віктор Шереш <sheregi1000@gmail.com>
2019-10-29 21:38:55 +0200

    Autopropeties

---
+++ 010_OOP1/015_AutoProperties/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_AutoProperties
+{
+
+    class Authors
+    {
+        public string Name { get; set; }
+        public string Book { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Authors authors = new Authors()
+            {
+                Name = "Jeffrey Richter",
+                Book = "CLR via C#"
+            };
+
+            Authors authors2 = new Authors
+            {
+                Name = "Steve McConnel",
+                Book = "Code Complete"
+            };
+            Console.WriteLine("Name - {0}, Book - {1}",authors.Name, authors.Book);
+            Console.WriteLine("Name - {0}, Book - {1}",authors2.Name, authors2.Book);
+        }
+    }
+}
