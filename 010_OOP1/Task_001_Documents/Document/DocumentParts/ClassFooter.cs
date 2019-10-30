64b3ecef5522a77cbdc3d4452b6e18b7b8461f27
Віктор Шереш <sheregi1000@gmail.com>
2019-10-30 09:50:41 +0200

    Added two class: footer and body

---
+++ 010_OOP1/Task_001_Documents/Document/DocumentParts/ClassFooter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents
+{
+    class ClassFooter
+    {
+        private string footerName;
+
+        public string Footer
+        {
+            get 
+            {
+                if (footerName !=null)
+                {
+                    return footerName;
+                }
+                else
+                {
+                    return "The footer is empty";
+                }
+            }
+
+            set
+            {
+                footerName = value;
+            }
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Footer);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
