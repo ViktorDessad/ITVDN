2e505895108b7fe983c2bacee2912a2d926514a3
Віктор Шереш <sheregi1000@gmail.com>
2019-10-31 19:18:56 +0200

    Parameterless constructor

---
+++ 010_OOP1/Task_004/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_004
+{
+    class Program
+    {
+        public static void Main()
+        {
+            MyClass myClass = new MyClass();
+
+            Console.WriteLine(myClass.X);
+        }
+    }
+
+    class MyClass
+    {
+        int x;
+
+        public int X { get { return x; } }
+
+        public MyClass()
+        {
+        }
+
+        public MyClass(int x)
+        {
+            this.x = x;
+        }
+    }
+}
