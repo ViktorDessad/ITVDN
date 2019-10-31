dc169c74571b517f8666bd598c6b2b3b373df6d1
Віктор Шереш <sheregi1000@gmail.com>
2019-10-31 18:59:29 +0200

    Task002

---
+++ 010_OOP1/Task_002/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_002
+{
+    class MyClass
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(name))
+                    return "Name is not defined";
+                else
+                    return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            my.Name = "test";
+            Console.WriteLine(my.Name);
+            Console.ReadKey();
+        }
+    }
+}
