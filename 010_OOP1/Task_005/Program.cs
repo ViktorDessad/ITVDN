5ab7c42a74c6fcbc163b40e920b2421ea325b835
Віктор Шереш <sheregi1000@gmail.com>
2019-11-01 15:58:30 +0200

    Methods and Constructors

---
+++ 010_OOP1/Task_005/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_005
+{
+    class Test
+    {
+        public int a = 99;
+        public string b = "C#";
+        public double c = Math.PI;
+
+        public Test()
+        {
+
+        }
+
+        public Test(int a, string b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void VoidMethod()
+        {
+            Console.WriteLine(a);
+        }
+
+        public string StringMethod()
+        {
+            return b;
+        }
+
+        public double DoubleMethod()
+        {
+            return c;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test(1099, "Hello Viktor", 3.14);
+            Test test2 = new Test();
+            test.VoidMethod();
+            string str = test.StringMethod();
+            double dbl = test.DoubleMethod();
+            Console.WriteLine($"{str},{dbl}");
+            Console.WriteLine($"{test2.a},{test2.b}, {test2.c}");
+        }
+    }
+}
