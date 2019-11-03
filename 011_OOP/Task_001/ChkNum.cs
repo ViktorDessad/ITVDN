bbbbaeb4fd729e27c6f09d35cba3bf0f098182aa
Віктор Шереш <sheregi1000@gmail.com>
2019-11-03 22:09:56 +0200

    Number checker

---
+++ 011_OOP/Task_001/ChkNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_001
+{
+    class ChkNum
+    {
+        // This method returned "true", if parameter "x" equals prime number
+        public bool IsPrime(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= x / i; i++)
+            {
+                if ((x % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ 011_OOP/Task_001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChkNum num = new ChkNum();
+
+            for (int i = 2; i < 10; i++)
+            {
+                if (num.IsPrime(i))
+                {
+                    Console.WriteLine($"{i} - prime number");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} - composite number");
+                }
+            }
+        }
+    }
+}
