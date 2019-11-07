fcbe3d46914dbb508edfe1a39ec1745db0eab9a1
Віктор Шереш <sheregi1000@gmail.com>
2019-11-07 18:21:10 +0200

    Encapsulation

---
+++ 011_OOP/003_Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Encapsulation
+{
+    class MyClass
+    {
+        MyClass my = null;
+
+        private void MyMethod()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void CallMethod()
+        {
+            my = new MyClass();
+
+
+            my.MyMethod();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            my.CallMethod();
+            
+        }
+    }
+}
