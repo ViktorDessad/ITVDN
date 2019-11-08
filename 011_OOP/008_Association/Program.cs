1733e85e7b1aad442e069205ef98b5918abe6ed8
Віктор Шереш <sheregi1000@gmail.com>
2019-11-08 17:51:25 +0200

    Association (not fabric method)

---
+++ 011_OOP/008_Association/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Association
+{
+
+    class Field
+    {
+        public void MethodField()
+        {
+            Console.WriteLine("Method Field");
+        }
+
+        public Field()
+        {
+            Console.WriteLine("Method Field");
+        }
+    }
+
+    class CallField
+    {
+        private Field field;
+
+        public void Field()
+        {
+            field = new Field();
+        }
+
+        public void Field2()
+        {
+            field.MethodField();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallField callField = new CallField();
+            callField.Field();
+            callField.Field2();
+
+        }
+    }
+}
