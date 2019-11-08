2f2051e9ce3cdaa156160727b975bc8b524fae11
Віктор Шереш <sheregi1000@gmail.com>
2019-11-08 17:40:36 +0200

    Association (fabric method)

---
+++ 011_OOP/007_Association_(fabric_method)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Association__fabric_method_
+{
+
+    class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product");
+        }
+    }
+
+    class Fabric
+    {
+        public Product ReturnProduct()
+        {
+            return new Product();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fabric fabric = new Fabric();
+            Product product = fabric.ReturnProduct();
+            
+        }
+    }
+}
