a45d64cb03c096bff08d05c64e5a63967ddfcc31
Віктор Шереш <sheregi1000@gmail.com>
2019-11-08 11:59:56 +0200

    Process is terminated due to StackOverflowException

---
+++ 011_OOP/005_SelfAssociation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _005_SelfAssociation
+{
+    class MyClass1
+    {
+        public int Field { get; } = 10;
+
+        MyClass1 my;
+        public MyClass1(int x)
+        {
+            my = new MyClass1(x);
+        }
+
+        public void My()
+        {
+            Console.WriteLine(my.Field);
+        }
+    }
+
+    class MyClass2
+    {
+        MyClass1 my;
+
+        public void MyMethod(int x)
+        {
+            my = new MyClass1(x);
+            my.My();
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyClass2 my = new MyClass2();
+            my.MyMethod(1);
+            // Process is terminated due to StackOverflowException.
+        }
+    }
+}
