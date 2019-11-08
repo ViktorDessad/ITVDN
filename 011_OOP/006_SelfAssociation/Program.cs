631bc7fc48a38c87b12308ff806e6fcd67254317
Віктор Шереш <sheregi1000@gmail.com>
2019-11-08 17:40:54 +0200

    SelfAssociation

---
+++ 011_OOP/006_SelfAssociation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_SelfAssociation
+{
+    class MyClass
+    {
+        //StackOverflowExeption
+        MyClass field;
+
+        public MyClass()
+        {            
+            Console.WriteLine($"Hello");
+            field = new MyClass();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            
+        }
+    }
+}
