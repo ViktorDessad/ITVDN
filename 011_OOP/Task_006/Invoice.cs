45fe932fa588d61268bc914bb3d7e46bcc97e3c4
Віктор Шереш <sheregi1000@gmail.com>
2019-11-09 17:43:20 +0200

    Article quantity

---
+++ 011_OOP/Task_006/Invoice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_006
+{
+    class Invoice
+    {
+        private readonly int account = 10;
+        private readonly string customer = "Viktor";
+        private readonly string provider = "Microsoft";
+
+        private string article;
+        private int quantity;
+
+        public void Vat(string article, int quantity, double sPrice, double vat)
+        {
+            this.article = article;
+            this.quantity = quantity;
+
+            double fullPrice = quantity * sPrice * vat;
+
+            Console.WriteLine($"account - {account}, customer - {customer}, provider - {provider}");
+            Console.WriteLine($"(With VAT) " +
+                $"Article - {this.article}, Quantity - {this.quantity}, " +
+                $"Standart price {sPrice}, full price {fullPrice}");
+        }
+        public void Vat(string article, int quantity, double sPrice)
+        {
+            this.article = article;
+            this.quantity = quantity;
+
+            double fullPrice = quantity * sPrice;
+
+            Console.WriteLine($"account - {account}, customer - {customer}, provider - {provider}");
+            Console.WriteLine($"(Without VAT) " +
+                $"Article - {this.article}, Quantity - {this.quantity}, " +
+                $"Standart price {sPrice}, full price {fullPrice}");
+        }
+
+    }
+}
---
+++ 011_OOP/Task_006/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice();
+            invoice.Vat("Book", 10, 10.34);
+            System.Console.WriteLine(new string('-', 60));
+            invoice.Vat("Magazine", 8, 6.45, 0.2);
+        }
+    }
+}
