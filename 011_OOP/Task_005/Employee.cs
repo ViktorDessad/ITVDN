41c909b9977548c54f1d23bb1d9cae80aa8057cc
Віктор Шереш <sheregi1000@gmail.com>
2019-11-09 16:40:20 +0200

    Employee salary calc

---
+++ 011_OOP/Task_005/Employee.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Task_005
+{
+    class Employee
+    {
+        private readonly string firstName = null;
+        private readonly string secondName = null;
+        public double standartSalary { set; get; } = 10000;
+        public double fullSalary { set; get; }
+        public double positionCoef { set; get; }
+        public double Fee { set; get; }
+     
+
+        public Employee(string firstName, string secondName)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        public void EmployeeSalary(string position, int yearsExp)
+        {
+            if (position == "director" || position == "Director")
+            {
+                if (yearsExp >= 1 && yearsExp < 5)
+                {
+                    Fee = 0.15;
+                    positionCoef = 10.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+
+                else if (yearsExp >= 5 && yearsExp < 10)
+                {
+                    Fee = 0.25;
+                    positionCoef = 13.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+                else if (yearsExp >= 10)
+                {
+                    Fee = 0.35;
+                    positionCoef = 18.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+            }
+
+            else if (position == "Administrator" || position == "administrator")
+            {
+                if (yearsExp >= 1 && yearsExp < 5)
+                {
+                    Fee = 0.15;
+                    positionCoef = 5.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+
+                else if (yearsExp >= 5 && yearsExp < 10)
+                {
+                    Fee = 0.25;
+                    positionCoef = 6.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+                else if (yearsExp >= 10)
+                {
+                    Fee = 0.35;
+                    positionCoef = 7.25;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }                
+            }
+            else if (position == "Worker" || position == "worker")
+            {
+                if (yearsExp >= 1 && yearsExp < 5)
+                {
+                    Fee = 0.15;
+                    positionCoef = 3.5;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+
+                else if (yearsExp >= 5 && yearsExp < 10)
+                {
+                    Fee = 0.25;
+                    positionCoef = 4.5;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+                else if (yearsExp >= 10)
+                {
+                    Fee = 0.35;
+                    positionCoef = 5.5;
+                    fullSalary = standartSalary * Fee * positionCoef;
+                    Console.WriteLine($"{position} - {firstName} {secondName} Standart salary = {standartSalary}");
+                    Console.WriteLine($"Full Salary = {fullSalary}, Fee - {Fee} and PositionCoef {positionCoef}");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ 011_OOP/Task_005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Viktor";
+            string sName = "Sheresh";
+            Employee employee = new Employee(fName, sName);
+            employee.EmployeeSalary("Director", 12);
+            Console.WriteLine(new string('*', 60));
+            employee.EmployeeSalary("administrator", 7);
+            Console.WriteLine(new string('*', 60));
+            employee.EmployeeSalary("Worker", 3);
+        }
+    }
+}
