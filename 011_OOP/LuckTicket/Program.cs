db78f5d3f28acf2198539245dc411c5c2fecfb00
Віктор Шереш <sheregi1000@gmail.com>
2019-11-13 18:17:41 +0200

    LuckyTicket

---
+++ 011_OOP/LuckTicket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Less02_task02
+{
+    public class MyClass
+    {
+        public bool IsLuckyTicket(string ticket)
+        {
+            if (ticket.Length != 6)
+                return false;
+            int number;
+            if (!Int32.TryParse(ticket, out number))
+                return false;
+            return (SumDigits(number / 1000) == SumDigits(number % 1000));
+        }
+        public static int SumDigits(int number)
+        {
+            string s = number.ToString();
+            if (s.Length == 1)
+                s = s.Insert(0, "00");
+            else if (s.Length == 2)
+                s = s.Insert(0, "0");
+            return Convert.ToInt32(s[0].ToString()) + Convert.ToInt32(s[1].ToString()) + Convert.ToInt32(s[2].ToString());
+        }
+    }
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            Console.WriteLine(my.IsLuckyTicket("123007"));
+            Console.WriteLine(my.IsLuckyTicket("123006"));            
+        }
+    }
+}
---
+++ 011_OOP/StackOverflowExeption/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowExeption
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+            MyClass field = new MyClass();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1");
+            MyClass inst = new MyClass();
+            Console.Write("2");
+            Console.ReadKey();
+        }
+    }
+}
