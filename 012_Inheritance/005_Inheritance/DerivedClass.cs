3c81d10e5f4ee5fd6894facdb95138c9e54802ed
Віктор Шереш <sheregi1000@gmail.com>
2019-11-24 18:04:58 +0200

    UpCast and DownCast sample

---
+++ 012_Inheritance/005_Inheritance/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Inheritance
+{
+    class DerivedClass : BaseClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method from Derived class");
+        }
+    }
+}
---
+++ 012_Inheritance/005_Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method();
+
+            //UpCast
+            // Приведение к базовому типу, все методы и поля из производного
+            // класса не будут доступны в базовом классе
+            BaseClass baseClass = derivedClass;
+            baseClass.Method();
+
+            //DownCast
+            //DownCast - невозможен без предварительного UpCast
+            DerivedClass derivedClass1 = (DerivedClass)baseClass;
+            derivedClass1.Method();
+
+
+            
+        }
+    }
+}
